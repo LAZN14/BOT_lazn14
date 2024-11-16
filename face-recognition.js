5a6136509927c1ea2eee4b93290b76710d4b0431
LAZN14 <vanya.sivakov2007@gmail.com>
2024-11-17 00:08:59 +0300

    камера фикс

--- face-recognition.js
+++ face-recognition.js
@@ -7,6 +7,10 @@ class FaceIDHandler {
     }
 
     async initCamera() {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            throw new Error('Ваше устройство не поддерживает доступ к камере');
+        }
+
         this.video = document.createElement('video');
         this.canvas = document.createElement('canvas');
         
@@ -18,6 +22,7 @@ class FaceIDHandler {
 
         try {
             this.model = await blazeface.load();
+            console.log('Модель загружена');
             
             this.stream = await navigator.mediaDevices.getUserMedia({ 
                 video: { 
@@ -26,36 +31,54 @@ class FaceIDHandler {
                     height: { ideal: 720 }
                 } 
             });
+            console.log('Доступ к камере получен');
+
             this.video.srcObject = this.stream;
-            await this.video.play();
+            return new Promise((resolve) => {
+                this.video.onloadedmetadata = () => {
+                    this.video.play().then(resolve);
+                };
+            });
         } catch (err) {
-            console.error("Ошибка:", err);
-            throw err;
+            console.error("Ошибка инициализации камеры:", err);
+            throw new Error(err.message || 'Ошибка доступа к камере');
         }
     }
 
     async captureFace() {
-        const predictions = await this.model.estimateFaces(this.video, false);
-        
-        if (predictions.length === 0) {
-            throw new Error("Лицо не обнаружено");
+        if (!this.video || !this.model) {
+            throw new Error('Камера не инициализирована');
         }
 
-        const context = this.canvas.getContext('2d');
-        this.canvas.width = this.video.videoWidth;
-        this.canvas.height = this.video.videoHeight;
-        context.drawImage(this.video, 0, 0);
-        
-        const imageData = this.canvas.toDataURL('image/jpeg', 1.0);
-        return {
-            image: imageData,
-            faceData: predictions[0]
-        };
+        try {
+            const predictions = await this.model.estimateFaces(this.video, false);
+            
+            if (predictions.length === 0) {
+                throw new Error("Лицо не обнаружено");
+            }
+
+            const context = this.canvas.getContext('2d');
+            this.canvas.width = this.video.videoWidth;
+            this.canvas.height = this.video.videoHeight;
+            context.drawImage(this.video, 0, 0);
+            
+            const imageData = this.canvas.toDataURL('image/jpeg', 1.0);
+            return {
+                image: imageData,
+                faceData: predictions[0]
+            };
+        } catch (err) {
+            console.error("Ошибка при захвате лица:", err);
+            throw err;
+        }
     }
 
     stopCamera() {
         if (this.stream) {
-            this.stream.getTracks().forEach(track => track.stop());
+            this.stream.getTracks().forEach(track => {
+                track.stop();
+                console.log('Камера остановлена');
+            });
         }
     }
 } 
